2dcd9177f2d5cc0279f078280563ed5a24d0f71b
BogdanDomashenko <got.bogdan@gmail.com>
2022-10-20 13:55:00 +0300

    upd

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://damp-waters-79332.herokuapp.com";
+export const API_URL = "https://pizza-api-release.vercel.app/";
 
 export const PHONE_NUMBER = "+38 (099) 222 33 11";
