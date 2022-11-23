53e575461f832a45b091b5693904da0c741ae285
Bohdan Domashenko <got.bogdan@gmail.com>
2022-11-23 13:32:42 +0200

    upd

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://pizza-api-release-production.up.railway.app";
+export const API_URL = "https://pizza-api-release.vercel.app";
 
 export const PHONE_NUMBER = "+38 (099) 222 33 11";
